b9ecc1fa00ce18c525845d87da205e8e35c60c5a
Danniely Santos <dannielyingrid@gmail.com>
2020-08-05 05:05:03 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,17 +3,16 @@
 def countdown(number)
   while number > 0 
   puts "#{number} SECOND(S)!"
-  number -= 1 
-end 
-return "HAPPY NEW YEAR!"
+    number -= 1 
+  end 
+  puts "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(number)
-  while number > 0 
-  puts "#{number} SECOND(S)!"
-  number -= 1 
-  sleep(1)
-end 
-
-return "HAPPY NEW YEAR!"
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1 
+    sleep(1)
+  end 
+  puts "HAPPY NEW YEAR!"
 end
